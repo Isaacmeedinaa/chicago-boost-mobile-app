d53a6261d531fce40d14986275539dbbe203bfa5
Isaac Medina <isaac.meedinaa@gmail.com>
2021-03-04 16:39:49 -0600

    finished notifications

--- app/Main.js
+++ app/Main.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AppState } from "react-native";
 import { registerRootComponent } from "expo";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
 import App from "./App";
 
+import * as Notifications from "expo-notifications";
+
 const Main = () => {
+  const [appState] = useState(AppState.currentState);
+
+  useEffect(() => {
+    AppState.addEventListener("change", removeNotificationBadgeCount);
+
+    return () => {
+      AppState.removeEventListener("change", removeNotificationBadgeCount);
+    };
+  }, []);
+
+  const removeNotificationBadgeCount = () => {
+    if (appState.match("active")) {
+      Notifications.setBadgeCountAsync(0);
+    }
+  };
+
   return (
     <Provider store={store}>
       <App />
